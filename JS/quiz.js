eba6397a57923edf5f6dc5e58f402f571827b14d
snowfield <qjsclzh123@naver.com>
2022-09-17 16:34:59 +0900

    Big

---
+++ JS/quiz.js
@@ -0,0 +1,85 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [
+  {
+    question: "가장 좋은 음식은?",
+    choice1: "햄버거",
+    choice2: "피자",
+    choice3: "떡볶이&모둠튀김&어묵",
+    choice4: "초밥",
+    answer: 1,
+  },
+  {
+    question: "재밌는 놀이는?",
+    choice1: "오목",
+    choice2: "PUBG",
+    choice3: "ZeroSivert",
+    choice4: "LOL",
+    answer: 3,
+  },
+  {
+    question: "정말 예쁜 shoe는?",
+    choice1: "Jordan 1 Low",
+    choice2: "Dunk High",
+    choice3: "Dunk Low",
+    choice4: "Jordan 1 High",
+    answer: 4,
+  },
+];
+//절대변수
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    return window.location.assign("/b.html");
+  }
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+startGame();
