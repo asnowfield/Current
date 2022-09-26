810e11d7ae59f6147fc2632660e553a1e9c490a6
snowfield <qjsclzh123@naver.com>
2022-09-27 08:03:52 +0900

    row col

--- JS/highscore.js
+++ JS/highscore.js
@@ -6,3 +6,8 @@ highScoresList.innerHTML = highScores
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
   })
   .join("");
+
+function clear(){
+  localStorage.clear();
+  window.location.assign('../highscore.html');
+}
