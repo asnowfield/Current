8c8d7f8d4e8f5408eca43b3214db45d1dcf169ea
snowfield <qjsclzh123@naver.com>
2022-09-28 12:28:00 +0900

    clear

--- JS/highscore.js
+++ JS/highscore.js
@@ -6,3 +6,9 @@ highScoresList.innerHTML = highScores
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
   })
   .join("");
+
+function clear_btn() {
+  localStorage.clear();
+  window.location.assign('../html/highscore.html');
+  alert("CLLLEARR!!!!")
+}
--- JS/sf.js
+++ JS/sf.js
@@ -38,7 +38,7 @@ startGame = () => {
 getNewQuestion = () => {
 	if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
 		localStorage.setItem('mostRecentScore', score);
-		return window.location.assign('../html/ending.html');
+		return window.location.assign('../html/conse.html');
 	}
 	questionCounter++;
 	progressText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
