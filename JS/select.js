44286305b6abc40d634fc10b998cbe1a84136f35
snowfield <qjsclzh123@naver.com>
2022-10-03 13:52:54 +0900

    silingking

--- JS/select.js
+++ JS/select.js
@@ -1,153 +1,57 @@
-function am_ez() {
-  localStorage.setItem('quizcode', 'am_ez');
+function am() {
+  localStorage.setItem('quizcode', '27');
 }
-function am_nm() {
-  localStorage.setItem('quizcode', 'am_nm');
+function art() {
+  localStorage.setItem('quizcode', '25');
 }
-function am_hd() {
-  localStorage.setItem('quizcode', 'am_hd');
+function book() {
+  localStorage.setItem('quizcode', '10');
 }
-function art_ez() {
-  localStorage.setItem('quizcode', 'art_ez');
+function cele() {
+  localStorage.setItem('quizcode', '26');
 }
-function art_nm() {
-  localStorage.setItem('quizcode', 'art_nm');
+function cs() {
+  localStorage.setItem('quizcode', '18');
 }
-function art_hd() {
-  localStorage.setItem('quizcode', 'art_hd');
+function film() {
+  localStorage.setItem('quizcode', '11');
 }
-function book_ez() {
-  localStorage.setItem('quizcode', 'book_ez');
+function gener() {
+  localStorage.setItem('quizcode', '9');
 }
-function book_nm() {
-  localStorage.setItem('quizcode', 'book_nm');
+function gm() {
+  localStorage.setItem('quizcode', '15');
 }
-function book_hd() {
-  localStorage.setItem('quizcode', 'book_hd');
+function gp() {
+  localStorage.setItem('quizcode', '22');
 }
-function cele_ez() {
-  localStorage.setItem('quizcode', 'cele_ez');
+function mt() {
+  localStorage.setItem('quizcode', '19');
 }
-function cele_nm() {
-  localStorage.setItem('quizcode', 'cele_nm');
+function musical() {
+  localStorage.setItem('quizcode', '13');
 }
-function cele_hd() {
-  localStorage.setItem('quizcode', 'cele_hd');
+function mz() {
+  localStorage.setItem('quizcode', '12');
 }
-function cs_ez() {
-  localStorage.setItem('quizcode', 'cs_ez');
+function sn() {
+  localStorage.setItem('quizcode', '17');
 }
-function cs_nm() {
-  localStorage.setItem('quizcode', 'cs_nm');
+function sp() {
+  localStorage.setItem('quizcode', '21');
 }
-function cs_hd() {
-  localStorage.setItem('quizcode', 'cs_hd');
+function tv() {
+  localStorage.setItem('quizcode', '14');
 }
-function film_ez() {
-  localStorage.setItem('quizcode', 'film_ez');
+function vehi() {
+  localStorage.setItem('quizcode', '28');
 }
-function film_nm() {
-  localStorage.setItem('quizcode', 'film_nm');
+function ez(){
+  localStorage.setItem('level','easy');
 }
-function film_hd() {
-  localStorage.setItem('quizcode', 'film_hd');
+function nm(){
+  localStorage.setItem('level','medium');
 }
-function gener_ez() {
-  localStorage.setItem('quizcode', 'gener_ez');
-}
-function gener_nm() {
-  localStorage.setItem('quizcode', 'gener_nm');
-}
-function gener_hd() {
-  localStorage.setItem('quizcode', 'gener_hd');
-}
-function gm_ez() {
-  localStorage.setItem('quizcode', 'gm_ez');
-}
-function gm_nm() {
-  localStorage.setItem('quizcode', 'gm_nm');
-}
-function gm_hd() {
-  localStorage.setItem('quizcode', 'gm_hd');
-}
-function gp_ez() {
-  localStorage.setItem('quizcode', 'gp_ez');
-}
-function gp_nm() {
-  localStorage.setItem('quizcode', 'gp_nm');
-}
-function gp_hd() {
-  localStorage.setItem('quizcode', 'gp_hd');
-}
-function his_ez() {
-  localStorage.setItem('quizcode', 'his_ez');
-}
-function his_nm() {
-  localStorage.setItem('quizcode', 'his_nm');
-}
-function his_hd() {
-  localStorage.setItem('quizcode', 'his_hd');
-}
-function mt_ez() {
-  localStorage.setItem('quizcode', 'mt_ez');
-}
-function mt_nm() {
-  localStorage.setItem('quizcode', 'mt_nm');
-}
-function mt_hd() {
-  localStorage.setItem('quizcode', 'mt_hd');
-}
-function musical_ez() {
-  localStorage.setItem('quizcode', 'musical_ez');
-}
-function musical_nm() {
-  localStorage.setItem('quizcode', 'musical_nm');
-}
-function musical_hd() {
-  localStorage.setItem('quizcode', 'musical_hd');
-}
-function mz_ez() {
-  localStorage.setItem('quizcode', 'mz_ez');
-}
-function mz_nm() {
-  localStorage.setItem('quizcode', 'mz_nm');
-}
-function mz_hd() {
-  localStorage.setItem('quizcode', 'mz_hd');
-}
-function sn_ez() {
-  localStorage.setItem('quizcode', 'sn_ez');
-}
-function sn_nm() {
-  localStorage.setItem('quizcode', 'sn_nm');
-}
-function sn_hd() {
-  localStorage.setItem('quizcode', 'sn_hd');
-}
-function sp_ez() {
-  localStorage.setItem('quizcode', 'sp_ez');
-}
-function sp_nm() {
-  localStorage.setItem('quizcode', 'sp_nm');
-}
-function sp_hd() {
-  localStorage.setItem('quizcode', 'sp_hd');
-}
-function tv_ez() {
-  localStorage.setItem('quizcode', 'tv_ez');
-}
-function tv_nm() {
-  localStorage.setItem('quizcode', 'tv_nm');
-}
-function tv_hd() {
-  localStorage.setItem('quizcode', 'tv_hd');
-}
-function vehi_ez() {
-  localStorage.setItem('quizcode', 'vehi_ez');
-}
-function vehi_nm() {
-  localStorage.setItem('quizcode', 'vehi_nm');
-}
-function vehi_hd() {
-  localStorage.setItem('quizcode', 'vehi_hd');
+function hd(){
+  localStorage.setItem('level','hard');
 }
